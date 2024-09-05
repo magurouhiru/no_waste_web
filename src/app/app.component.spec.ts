dd18487186f3d427ad51d520be41c625a3be6333
asdflkj169 <asdflkj169@gmail.com>
2024-09-05 20:20:55 +0900

    install primeng tailwind -D jest jest-preset-angular @types/jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('no_waste_web');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, no_waste_web');
-  });
 });
